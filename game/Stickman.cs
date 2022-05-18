169590bc3ab299033c73d1e5873d53d35c541135
Reed Cannon <reed.cannon97@gmail.com>
2022-05-18 13:05:49 -0600

    Fixed backslashes

--- game/Stickman.cs
+++ game/Stickman.cs
@@ -3,24 +3,34 @@ namespace Parachute {
     public class Stickman {
 
     
-        string completeStickman = {
-       "        ____________            \n"     ,
-       "      /    |    |    \t          \n"     ,
-       "     /     |    |     \t         \n"     ,
-       "     \t     |    |     /         \n"     ,
-       "      \t    |    |    /          \n"     ,
-       "       \t   |    |   /           \n"     ,
-       "        \t  |    |  /            \n"     ,
-       "         \t |    | /             \n"     ,
-       "          \t|    |/              \n"     ,
-       "           |----|               \n"     ,
-       "           | (O)|               \n"     ,
-       "           |  | |               \n"     ,
-       "           |----|               \n"     ,
-       "              |                 \n"     ,
-       "             / \t                \n"     ,
-       "            /   \t               \n"     };
-        
+        string completeStickman =       "        ____________            \n"     +
+       "      /    |    |    \\          \n"     +
+       "     /     |    |     \\         \n"     +
+       "     \\     |    |     /         \n"     +
+       "      \\    |    |    /          \n"     +
+       "       \\   |    |   /           \n"     +
+       "        \\  |    |  /            \n"     +
+       "         \\ |    | /             \n"     +
+       "          \\|    |/              \n"     +
+       "           |----|               \n"     +
+       "           | (O)|               \n"     +
+       "           |  | |               \n"     +
+       "           |----|               \n"     +
+       "              |                 \n"     +
+       "             / \\                \n"     +
+       "            /   \\               \n"     ;
+
+        public Stickman()
+        {
+
+        }
+
+        public void drawCompleteStickman()
+        {
+            Console.WriteLine(completeStickman);
+        }
+
     }
+    
     
 }
