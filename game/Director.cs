40f1912f4311accae5951201e59bb8ec97d0c8b4
Reed Cannon <reed.cannon97@gmail.com>
2022-05-18 10:58:42 -0600

    UPdated director template

---
+++ game/Director.cs
@@ -0,0 +1,52 @@
+namespace Parachute
+{
+    /// <summary>
+    /// <para>A person who directs the game.</para>
+    /// <para>
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </para>
+    /// </summary>
+    public class Director
+    {
+        private bool isPlaying = true;
+        private TerminalService terminalService = new TerminalService();
+
+        /// <summary>
+        /// Constructs a new instance of Director.
+        /// </summary>
+        public Director()
+        {
+        }
+
+        /// <summary>
+        /// Starts the game by running the main game loop.
+        /// </summary>
+        public void StartGame()
+        {
+            while (isPlaying)
+            {
+                GetInputs();
+                DoUpdates();
+                DoOutputs();
+            }
+        }
+
+        
+        private void GetInputs()
+        {
+            
+        }
+
+        
+        private void DoUpdates()
+        {
+            
+        }
+
+        
+        private void DoOutputs()
+        {
+            
+        }
+    }
+}
