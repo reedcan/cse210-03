49f5fa9051b9b80012a5ea439c25b3e0a6d4d2a1
Reed Cannon <reed.cannon97@gmail.com>
2022-05-18 12:24:13 -0600

    Changed class declaration to reflect  name change

---
+++ game/Stickman.cs
@@ -0,0 +1,26 @@
+//Damian's domainian
+namespace Parachute {
+    public class Stickman {
+
+    
+        string completeStickman = {
+       "        ____________            \n"     ,
+       "      /    |    |    \t          \n"     ,
+       "     /     |    |     \t         \n"     ,
+       "     \t     |    |     /         \n"     ,
+       "      \t    |    |    /          \n"     ,
+       "       \t   |    |   /           \n"     ,
+       "        \t  |    |  /            \n"     ,
+       "         \t |    | /             \n"     ,
+       "          \t|    |/              \n"     ,
+       "           |----|               \n"     ,
+       "           | (O)|               \n"     ,
+       "           |  | |               \n"     ,
+       "           |----|               \n"     ,
+       "              |                 \n"     ,
+       "             / \t                \n"     ,
+       "            /   \t               \n"     };
+        
+    }
+    
+}
