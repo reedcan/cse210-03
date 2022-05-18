986577000addf1ff910bfb67fed0c50e3a16a432
Reed Cannon <reed.cannon97@gmail.com>
2022-05-18 10:57:04 -0600

    Added outline to classes

---
+++ game/Guesser.cs
@@ -0,0 +1,19 @@
+//Handles correct/incorrect guess logic, similar to Seeker from Hide and Seek
+
+using System;
+
+
+namespace Parachute
+{
+    
+    public class Guesser
+    {
+        
+        public Guesser()
+        {
+            
+        }
+
+
+    }
+}
---
+++ game/Parachuter.cs
@@ -0,0 +1,18 @@
+//Responsible for word generation and word display, in addition to displaying the parachuter. Similar to Hider Class from Hide and Seek
+
+using System;
+using System.Collections.Generic;
+
+
+namespace Parachute
+{
+    public class Parachuter
+    {
+       
+        public Parachuter()
+        {
+            
+        }
+
+    }
+}
