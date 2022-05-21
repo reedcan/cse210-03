5f504cffe74dd95685190fcff7d9c4977d7bdb03
ckcomin <com20009@byui.edu>
2022-05-20 18:31:23 -0600

    guess

--- game/Guesser.cs
+++ game/Guesser.cs
@@ -19,26 +19,40 @@ namespace Parachute
             // make list with blank lines
             // if guess matches letter then append letter to blank line list
         }
-        private string GetWord()
+        public List<string> GetWord()
          {
 
             // Call and return the getRandomWord function
             string word = words.getRandomWord();
             List<string>wordList = words.convertRandomWord(word);
-            foreach (string letter in word)
+            foreach (string letter in wordList)
             {
                 List<string>lines = new List<string>();
                 lines.Add("_");
+                return lines;
             }
-
-
-            return words.getRandomWord();
+            return wordList;
+            
          }
 
-         private string CheckGuess(guess)
+        private void CheckGuess(string guess, List<string> lines, List<string> wordList)
          {
              // if guess is in random generated word
              // then convert line into latter
+            
+            if (wordList.Contains(guess))
+            {
+                foreach (string line in lines);
+                {
+                    if (letter = guess)
+                    {
+                        string guess = line
+                        lines.Add(guess);
+                    }
+                }
+                    
+            }
+
 
          }
 
